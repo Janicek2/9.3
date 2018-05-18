e922b8826220198750aec747c45ef8a9ad6a8268
Janicek2 <bjanicki82@gmail.com>
2018-05-18 20:25:01 +0200

    Fix textNoComa

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@ var dinosaur = "Triceratops";
 var upperCaseDinosaur = dinosaur.toUpperCase();
 var arrayText = text.split(" ");
 arrayText.splice([0], 1, upperCaseDinosaur);
-var textNoComa = arrayText.join(' ');
-var shortText = textNoComa.slice(0, text.length / 2);
-console.log(textNoComa);
+var shortText = text.slice(0, text.length / 2);
+console.log(text);
 console.log(shortText);
 
 
